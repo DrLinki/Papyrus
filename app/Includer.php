7545c33d07af72c26a4b90477ba5b4f10314bd7f
Yannick SELLAMI <yannick.sellami@linki.fr>
2024-05-20 19:49:30 +0200

    base(structure): Adding Includer.php
    Adding the includer file of required classes

---
+++ app/Includer.php
@@ -0,0 +1,12 @@
+<?php
+require APP . DS . 'inc/Session.php';
+require APP . DS . 'inc/Pagination.php';
+require APP . DS . 'inc/Form.php';
+require APP . DS . 'inc/functions.php';
+require APP . DS . 'Router.php';
+require ROOT . DS . 'config' . DS . 'Conf.php';
+require APP . DS . 'Request.php';
+require APP . DS . 'Dispatcher.php';
+require SRC . DS . 'Controller.php';
+require SRC . DS . 'Repository.php';
+?>
