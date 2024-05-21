4503c31a99194d6bf07dc98cf82c9b70282382d1
Yannick SELLAMI <yannick_linki@hotmail.fr>
2024-05-21 11:23:16 +0200

    base(default): Create DefaultController and default/index
    Creation of default controller ans view for the application

---
+++ app/src/Controller/DefaultController.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Controller;
+
+use App\Controller;
+class DefaultController extends Controller{
+	public function index(){}
+}
+?>
---
+++ app/src/View/default/index.php
@@ -0,0 +1,3 @@
+<h1>Papyrus</h1>
+<h2>Welcome Page</h2>
+<p>This is the default page of your application.</p>
